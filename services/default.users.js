1f82dfd092416f11c7b61cfaa4c892a8500354d4
aijaz <rsi-aijaz@rsi.iballbatonwifi.com>
2020-06-19 11:28:00 +0530

    Requested changes

--- services/default.users.js
+++ services/default.users.js
@@ -3,10 +3,10 @@ const bcryptService = require('./bcrypt.service')
 const pass = bcryptService().password({ password: '123' })
 
 const defaultUsers = [
-    {"first_name": "Tom","last_name": "Hank","email": "tom@abc.com","password": pass, "latitude": "123", "longitude": "456", "image": "images/user.png"},
-    {"first_name": "Max","last_name": "Payne","email": "max@abc.com","password": pass, "latitude": "123", "longitude": "456", "image": "images/user.png"},
-    {"first_name": "John","last_name": "Doe","email": "john@abc.com","password": pass, "latitude": "123", "longitude": "456", "image": "images/user.png"},
-    {"first_name": "Tommy","last_name": "Maddison","email": "tommy@abc.com","password": pass, "latitude": "123", "longitude": "456", "image": "images/user.png"},
+    {"first_name": "Tom","last_name": "Hank","email": "tom@abc.com","password": pass, "latitude": null, "longitude": null, "image": "images/user.png"},
+    {"first_name": "Max","last_name": "Payne","email": "max@abc.com","password": pass, "latitude": null, "longitude": null, "image": "images/user.png"},
+    {"first_name": "John","last_name": "Doe","email": "john@abc.com","password": pass, "latitude": null, "longitude": null, "image": "images/user.png"},
+    {"first_name": "Tommy","last_name": "Maddison","email": "tommy@abc.com","password": pass, "latitude": null, "longitude": null, "image": "images/user.png"},
 ]
 
 const uploadDefaultUsers = async ()=>{
