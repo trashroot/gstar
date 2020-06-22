c6d74cfbcf11076a46de58137894e7d7e38cda4e
aijaz <rsi-aijaz@rsi.iballbatonwifi.com>
2020-06-22 19:28:38 +0530

    Updated default users list

--- services/default.users.js
+++ services/default.users.js
@@ -4,7 +4,7 @@ const pass = bcryptService().password({ password: '123' })
 
 const defaultUsers = [
     {"first_name": "Luis","last_name": "Moreno","email": "luis@globalstar.com","password": 123, "latitude": null, "longitude": null, "image": "images/user.png"},
-    {"first_name": "Luis1","last_name": "Moreno","email": "luis1@globalstar.com","password": 123, "latitude": null, "longitude": null, "image": "images/user.png"},
+    {"first_name": "Luis-M","last_name": "Moreno","email": "moreno@globalstar.com","password": 123, "latitude": null, "longitude": null, "image": "images/user.png"},
     {"first_name": "Ankit","last_name": "Chauhan","email": "ankit@rsystems.com","password": 123, "latitude": null, "longitude": null, "image": "images/user.png"},
     {"first_name": "Ashish","last_name": "Anjan","email": "anjan@rsystems.com","password": 123, "latitude": null, "longitude": null, "image": "images/user.png"},
     {"first_name": "Ashish","last_name": "Lavanya","email": "ashish@rsystems.com","password": 123, "latitude": null, "longitude": null, "image": "images/user.png"},
