5273c5b72467b4ab43aaba90e1c1404ebed3e586
aijaz <rsi-aijaz@rsi.iballbatonwifi.com>
2020-06-22 20:19:30 +0530

    Default user fixes

--- services/default.users.js
+++ services/default.users.js
@@ -3,11 +3,11 @@ const bcryptService = require('./bcrypt.service')
 const pass = bcryptService().password({ password: '123' })
 
 const defaultUsers = [
-    {"first_name": "Luis","last_name": "Moreno","email": "luis@globalstar.com","password": 123, "latitude": null, "longitude": null, "image": "images/user.png"},
-    {"first_name": "Luis-M","last_name": "Moreno","email": "moreno@globalstar.com","password": 123, "latitude": null, "longitude": null, "image": "images/user.png"},
-    {"first_name": "Ankit","last_name": "Chauhan","email": "ankit@rsystems.com","password": 123, "latitude": null, "longitude": null, "image": "images/user.png"},
-    {"first_name": "Ashish","last_name": "Anjan","email": "anjan@rsystems.com","password": 123, "latitude": null, "longitude": null, "image": "images/user.png"},
-    {"first_name": "Ashish","last_name": "Lavanya","email": "ashish@rsystems.com","password": 123, "latitude": null, "longitude": null, "image": "images/user.png"},
+    {"first_name": "Luis","last_name": "Moreno","email": "luis@globalstar.com","password": pass, "latitude": null, "longitude": null, "image": "images/user.png"},
+    {"first_name": "Luis-M","last_name": "Moreno","email": "moreno@globalstar.com","password": pass, "latitude": null, "longitude": null, "image": "images/user.png"},
+    {"first_name": "Ankit","last_name": "Chauhan","email": "ankit@rsystems.com","password": pass, "latitude": null, "longitude": null, "image": "images/user.png"},
+    {"first_name": "Ashish","last_name": "Anjan","email": "anjan@rsystems.com","password": pass, "latitude": null, "longitude": null, "image": "images/user.png"},
+    {"first_name": "Ashish","last_name": "Lavanya","email": "ashish@rsystems.com","password": pass, "latitude": null, "longitude": null, "image": "images/user.png"},
 ]
 
 const uploadDefaultUsers = async ()=>{
